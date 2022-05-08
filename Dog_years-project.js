91e40f2c5164b4be6b2b1d308563456cbf407edf
Showman_Das <Showmandas3216@gmail.com>
2022-05-08 17:08:15 +0600

    upload two mini project

---
+++ Dog_years-project.js
@@ -0,0 +1,26 @@
+//declare myage
+let myAge=23;
+
+//another variable declare
+var earlyYears=2;
+var earlyYears=2 * 10.5;
+
+//subtract 2 from myAge
+var laterYears=myAge-2;
+var laterYears=laterYears * 4;
+
+//For checking
+// console.log(earlyYears)
+// console.log(laterYears)
+
+//Add earlyYears and laterYears
+let myAgeInDogYears=earlyYears + laterYears;
+
+//store my name as string with loewCase method
+var myName="Shanu";
+myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am 
+${myAge} years old in human years which is 
+${myAgeInDogYears} years old in dog years.`)
+
---
+++ Kelvin-Weather-project.js
@@ -0,0 +1,11 @@
+//declare a variable
+const kelvin=0;
+
+//convert kelvin to celcius
+const celsius=273-kelvin
+
+//convert celsius to farenheit
+//to get round number use floor
+const fahrenheit =Math.floor(celsius * (9/5) + 32)
+
+console.log(`The temperature is ${fahrenheit}  degrees Fahrenheit.`)
